f9c3797df7b25bf1a764aaadfdac3eedf45bb28d
Mustafa Burak Kilic <burak.kilic@mobilistbilisim.com>
2022-05-02 00:00:18 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace IfElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time= DateTime.Now.Hour;
+
+            if (time<=11)
+            {
+                System.Console.WriteLine( "Sabah saatleri");
+            }
+            else if(time<=17)
+            {
+                System.Console.WriteLine("Öğle saatleri");
+            }
+            else if (time<=21)
+            {
+                System.Console.WriteLine("Akşam saatleri");
+            }
+            else
+            System.Console.WriteLine("Gece saatleri");
+        }
+    }
+}
